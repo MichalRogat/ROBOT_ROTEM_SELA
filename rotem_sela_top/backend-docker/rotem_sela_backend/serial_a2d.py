ab8392541fba99dd60ce32eed40787769643d77e
hilaRogat <134198579+hilaRogat@users.noreply.github.com>
2023-10-30 21:58:45 +0200

    telemetry values updated (check IMU) & a2d serial checksum

--- rotem_sela_top/backend-docker/rotem_sela_backend/serial_a2d.py
+++ rotem_sela_top/backend-docker/rotem_sela_backend/serial_a2d.py
@@ -11,17 +11,35 @@ class SerialA2D():
 
     def listen(self):    
         while True:
+                    calc_checksum = 0
+                    received_checksum = 0
                     received_data = self.ser.read(1)              #read serial port
                     if received_data[0] == 0xaa:
                         received_data = self.ser.read(1)    
                         if received_data[0] == 0xde:
-                            received_data = self.ser.read(21)
-                            self.values = list(struct.unpack('10h', received_data[0:20]))
+                            received_data = self.ser.read(1)    
+                            if received_data[0] == 21:
+                                received_data = self.ser.read(21)
+                                self.values = list(struct.unpack('10h', received_data[0:20]))
+                                calc_checksum = self.CalcChecsum(self.values)
+                                received_checksum = received_data[20]
+                                if(calc_checksum != received_checksum):
+                                    continue
+                            else:
+                                continue
                         else:
                             continue
                     else:
                         continue
     
+    def CalcChecsum(values):
+        MsgLen = 20
+        ret_val = 0xaa + 0xde + 21
+        for value in values:
+            ret_val += value
+        ret_val = ~ret_val
+        return ret_val            
+
 if __name__ == "__main__":
     x = SerialA2D()
     x.listen()
