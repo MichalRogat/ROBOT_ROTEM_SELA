a78b835f97c2f3345e27a4a00cd1130b3b7b8955
hilaRogat <hilap@rogat.co.il>
2023-11-12 11:30:17 +0000

    create linux service

---
+++ rotem_sela_top/backend-docker/rotem_sela_backend/start_my_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/rogat/ESP32-ROTEM-SELA/rotem_sela_top/backend-docker/rotem_sela_backend/.venv/bin/activate  # Activate the virtual environment
+/home/rogat/ESP32-ROTEM-SELA/rotem_sela_top/backend-docker/rotem_sela_backend/.venv/bin/python /home/rogat/ESP32-ROTEM-SELA/rotem_sela_top/backend-docker/rotem_sela_backend/new_video_main.py
