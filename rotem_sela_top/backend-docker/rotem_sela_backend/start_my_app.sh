1f30fa27efcbefd83e1fcc02a244fada21bf5542
hilaRogat <134198579+hilaRogat@users.noreply.github.com>
2023-11-12 13:57:25 +0200

    service instruction notes

--- rotem_sela_top/backend-docker/rotem_sela_backend/start_my_app.sh
+++ rotem_sela_top/backend-docker/rotem_sela_backend/start_my_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#chmod +x /path/to/start_my_app.sh
 source /home/rogat/ESP32-ROTEM-SELA/rotem_sela_top/backend-docker/rotem_sela_backend/.venv/bin/activate  # Activate the virtual environment
 /home/rogat/ESP32-ROTEM-SELA/rotem_sela_top/backend-docker/rotem_sela_backend/.venv/bin/python /home/rogat/ESP32-ROTEM-SELA/rotem_sela_top/backend-docker/rotem_sela_backend/new_video_main.py
